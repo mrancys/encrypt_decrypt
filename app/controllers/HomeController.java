c44379512c6a105be318c1cc1ca4b01cebc53716
Martynas <martynas.rancys@gmail.com>
2021-02-25 02:15:52 +0200

    WIP

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,16 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
 import play.mvc.*;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -8,22 +18,30 @@ import play.mvc.*;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+
+    public static final String APPLICATION_SECRET_KEY = "12345";
+
     public Result index() {
         return ok(views.html.index.render());
     }
 
-    public Result encrypt(Http.Request request) {
-        System.out.println(request.body());
-        return ok(views.html.index.render());
+    public Result encrypt(Http.Request request) throws NoSuchAlgorithmException, InvalidKeyException {
+        String valueToDigest = request.body().asJson().findPath("unencrypted").textValue();
+
+        byte[] key = APPLICATION_SECRET_KEY.getBytes();
+
+        HmacUtils hm = new HmacUtils();
+        String encrypted = hm.generateHmac256(valueToDigest, key);
+
+        ObjectNode result = Json.newObject();
+        result.put("encrypted", encrypted);
+        return ok(result);
     }
 
-    public Result decrypt() {
+    public Result decrypt(Http.Request request) {
+
         return ok(views.html.index.render());
     }
 }
+
+
